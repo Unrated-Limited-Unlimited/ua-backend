44a9c9d3f49455a17ee007b0e670d251c9d8fd40
Thomas-Johansen <thomassandjoh@hotmail.com>
2024-04-24 19:01:20 +0200

    Removed unused imports

--- src/main/kotlin/com/ulu/sorters/ResponseData.kt
+++ src/main/kotlin/com/ulu/sorters/ResponseData.kt
@@ -1,8 +1,6 @@
 package com.ulu.sorters
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class ResponseData(
