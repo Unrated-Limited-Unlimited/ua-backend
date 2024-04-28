40fbc79ba41426be447c1643bd4aaa8a07e5fd0d
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-04-28 15:49:29 +0200

    updated startServer scritp

--- startServer.sh
+++ startServer.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 export MICRONAUT_ENVIRONMENTS=prod
-gradle build clean && gradle build && java -jar build/libs/unrated-0.1-all.jar
+gradle build clean && gradle build
+docker build -t ua-backend .
+docker stop ua-backend
+docker remove ua-backend
+docker run --restart=always --name ua-backend -p 8000:8000 -d ua-backend:latest -e MICRONAUT_ENVIRONMENTS=prod -e DB_PASSWORD=$1 ADMIN_PASS=$1
+
